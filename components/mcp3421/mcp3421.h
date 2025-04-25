a629febeeb76f014b59f7ad5fc2446e51453a5e4
kakanics <affan.bb7@gmail.com>
2025-04-25 23:06:45 +0500

    remove redefinictions (added prototypes)

--- components/mcp3421/mcp3421.h
+++ components/mcp3421/mcp3421.h
@@ -7,19 +7,13 @@
 namespace esphome{
 namespace mcp3421{
   
-class MCP3421Sensor : public sensor::Sensor, public PollingComponent {  // Inheriting from sensor::Sensor
+class MCP3421Sensor : public sensor::Sensor{  // Inheriting from sensor::Sensor
  public:
  MCP3421Sensor() {}
 
- void setup() override {  // Defining setup inside the class
-  // Initialize the MCP3421 sensor (e.g., setup I2C, GPIOs)
-}
+ void setup();
 
-  void update() override {  // Defining update inside the class
-    // Read the sensor data and publish the values
-    float ph_value = this->read_ph_value();
-    this->publish_state(ph_value);
-  }
+  void update();
 
   float read_ph_value() {  // Correct placement of read_ph_value()
     // Add the logic to read the pH value from the sensor
