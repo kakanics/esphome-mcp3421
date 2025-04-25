365f4f8aab40e5f16abcff37da131e8affa79c7b
kakanics <affan.bb7@gmail.com>
2025-04-25 22:59:03 +0500

    correct the class name

--- components/mcp3421/mcp3421.h
+++ components/mcp3421/mcp3421.h
@@ -1,14 +1,14 @@
 #pragma once
 
-#include <esphome/core/component.h>
-#include <esphome/components/sensor/sensor.h>
-#include <esphome/components/i2c/i2c.h>
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/i2c/i2c.h"
 
 // Define the MCP3421 class
 using namespace esphome;
 using namespace mcp3421;
 
-class MCP3421 : public sensor::Sensor, public PollingComponent {  // Inheriting from sensor::Sensor
+class MCP3421Sensor : public sensor::Sensor, public PollingComponent {  // Inheriting from sensor::Sensor
  public:
   MCP3421() {}
 
